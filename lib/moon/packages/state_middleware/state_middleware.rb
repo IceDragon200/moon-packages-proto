16f64a94721d1c412f952cf80378d2d136042904
Corey Powell <mistdragon100@gmail.com>
2015-05-17 13:20:35 -0500

    state_middleware: Debugging and trimming

--- lib/moon/packages/state_middleware/state_middleware.rb
+++ lib/moon/packages/state_middleware/state_middleware.rb
@@ -1,9 +1,12 @@
+require 'moon-logfmt/load'
+
 class MiddlewareManager
   attr_reader :state
 
   EMPTY = []
 
   def initialize(state)
+    @logger = Moon::Logfmt::Logger.new ctx: 'MiddlewareManager'
     @state = state
     reset
   end
@@ -15,12 +18,16 @@ class MiddlewareManager
   end
 
   def refresh
-    puts "Refreshing Middlewares"
+    l = @logger.new state: state.to_s
+    l.write msg: 'Refreshing Middlewares'
     @middlewares.each do |middleware_klass|
-      puts "Refreshing: #{middleware_klass}"
+      ll = l.new middleware: middleware_klass
+      ll.write msg: "Refreshing Middleware"
       middleware = middleware_klass.new(state)
+      middleware.logger = ll
       @middleware_registry[middleware_klass] = middleware
       middleware.hooks.each do |hook|
+        ll.write hook: hook, msg: 'Adding hook'
         (@middleware_hooks[hook] ||= []).push(middleware)
       end
     end
@@ -38,12 +45,19 @@ class MiddlewareManager
 
   def trigger(hook, *args)
     (@middleware_hooks[hook] || EMPTY).each do |middleware|
-      middleware.send(hook, *args)
+      ## update hooks are noisy
+      #unless hook.to_s.include?('update')
+      #  @logger.write middleware: middleware.to_s, hook: hook, msg: 'Running Hook'
+      #end
+      middleware.trigger_hook(hook, *args)
     end
   end
 end
 
 module Middlewarable
+  class ReinitializationError < RuntimeError
+  end
+
   module ClassMethods
     prototype_attr :middleware
 
@@ -55,12 +69,14 @@ module Middlewarable
   attr_reader :middleware_manager
 
   def initialize(*args, &block)
-    init_middleware
+    initialize_middleware
     super(*args, &block)
   end
 
-  private def init_middleware
-    raise if @middleware_manager # re-initialization!
+  private def initialize_middleware
+    if @middleware_manager # re-initialization!
+      raise ReinitializationError, "MiddlewareManager was already initialized!"
+    end
     @middleware_manager = MiddlewareManager.new(self)
     @middleware_manager.setup self.class.all_middlewares
   end
@@ -73,61 +89,24 @@ end
 module StateMiddlewarable
   include Middlewarable
 
-  # hooks
-  def init
-    super
-    @middleware_manager.trigger :init
+  def self.hook_method(name)
+    define_method name do |*args, &block|
+      super(*args, &block)
+      @middleware_manager.trigger(name, *args)
+    end
   end
 
-  def start
-    super
-    @middleware_manager.trigger :start
-  end
-
-  def pause
-    super
-    @middleware_manager.trigger :pause
-  end
-
-  def unpause
-    super
-    @middleware_manager.trigger :unpause
-  end
-
-  def terminate
-    super
-    @middleware_manager.trigger :terminate
-  end
-
-  def pre_update(delta)
-    super
-    @middleware_manager.trigger :pre_update, delta
-  end
-
-  def update(delta)
-    super
-    @middleware_manager.trigger :update, delta
-  end
-
-  def post_update(delta)
-    super
-    @middleware_manager.trigger :post_update, delta
-  end
-
-  def pre_render
-    super
-    @middleware_manager.trigger :pre_render
-  end
-
-  def render
-    super
-    @middleware_manager.trigger :render
-  end
-
-  def post_render
-    super
-    @middleware_manager.trigger :post_render
-  end
+  hook_method :init
+  hook_method :start
+  hook_method :pause
+  hook_method :resume
+  hook_method :terminate
+  hook_method :pre_update
+  hook_method :update
+  hook_method :post_update
+  hook_method :pre_render
+  hook_method :render
+  hook_method :post_render
 
   def self.included(mod)
     mod.extend ClassMethods
@@ -142,13 +121,21 @@ class EventHopper
   def trigger(event)
     @target << event
   end
+
+  alias :call :trigger
 end
 
 class BaseMiddleware
+  attr_accessor :logger
   attr_reader :state
 
   def initialize(state)
+    @logger = Moon::Logfmt::NullLogger
     @state = state
+  end
+
+  def trigger_hook(hook, *args, &block)
+    public_send(hook, *args, &block)
   end
 
   def hooks
@@ -160,44 +147,31 @@ class BaseMiddleware
   end
 
   def self.hook(method_name)
-    hooks << method_name
+    if method_name.is_a?(Hash)
+      method_name.each do |key, target|
+        key = key.to_sym
+        target = target.to_sym
+        define_method(key) { |*args, &block| send(target, *args, &block) }
+        hooks << key
+      end
+    else
+      hooks << method_name
+    end
   end
 end
 
 class InputMiddleware < BaseMiddleware
   attr_reader :handle
 
-  private def register
-    state.engine.input.register @hopper
-  end
-
-  private def unregister
-    state.engine.input.unregister @hopper
+  private def setup_exception_handler
+    return unless state.respond_to?(:on_exception)
+    exh = ->(exc, backtrace) { state.on_exception(exc, backtrace) }
+    @handle.on_exception = exh
   end
 
   hook def init
-    @event_stack = []
-    @hopper = EventHopper.new @event_stack
     @handle = Moon::Input::Observer.new
-  end
-
-  hook def start
-    register
-  end
-
-  hook def pause
-    unregister
-  end
-
-  hook def unpause
-    register
-  end
-
-  hook def pre_update(delta)
-    until @event_stack.empty?
-      ev = @event_stack.shift
-      @handle.trigger ev
-    end
+    setup_exception_handler
   end
 end
 
