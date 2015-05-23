6f7985802f418843330a023782679abd194809ed
Corey Powell <mistdragon100@gmail.com>
2015-05-23 10:52:45 -0500

    state_mvc: Observable, Added #notify_all

--- lib/moon/packages/state_mvc/observable.rb
+++ lib/moon/packages/state_mvc/observable.rb
@@ -18,22 +18,33 @@ module Moon
     end
 
     module ClassMethods
+      extend Moon::Prototype
+
+      prototype_attr :observed_property
+
       def observe_attr(name)
+        observed_properties << name.to_sym
         setter_name = "#{name}=".to_sym
         unobserved_setter_name = "unobserved_#{setter_name}".to_sym
         alias_method unobserved_setter_name, setter_name
         define_method(setter_name) do |*args, &block|
           old = send(name)
           send(unobserved_setter_name, *args, &block)
-          notify(old, name)
+          notify(name, old)
         end
       end
     end
 
     include Moon::Eventable
 
-    def notify(old, attribute)
+    def notify(attribute, old)
       trigger { ObservedChange.new self, attribute, old, send(attribute) }
+    end
+
+    def notify_all
+      self.class.each_observed_property do |key|
+        notify(key, nil)
+      end
     end
 
     def self.included(mod)
