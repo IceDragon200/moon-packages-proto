3fc8bfa13dc5de7480fa111fa091435bda8684bc
Corey Powell <mistdragon100@gmail.com>
2015-09-21 09:47:21 -0500

    Added doc to String#to_duration

--- lib/moon/packages/es-aux/core_ext/string.rb
+++ lib/moon/packages/es-aux/core_ext/string.rb
@@ -1,4 +1,8 @@
 class String
+  # Parses the current string as a time notation see
+  # {Moon::TimeUtil.parse_duration}
+  #
+  # @return [Float]
   def to_duration
     Moon::TimeUtil.parse_duration(self)
   end
